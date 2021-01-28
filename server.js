72ef64f1d43e5d373271689b9743208556cb3a50
hoshi000 <saileshdulal2021@gmail.com>
2021-01-28 10:01:23 +0545

    added

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
-    ]
+];
+
 app.get('/users', function(req,res){
         res.json({
             success: true,
             message: 'successfully got users. Nice!',
             users: mockUserData
         })
+})
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
